c872aa131a17adba44f0e771efe5efcd66736068
tahir59 <52169987+tahir59@users.noreply.github.com>
2019-11-27 17:38:37 +0500

    Update vnet.tf

--- azure/vnet/vnet.tf
+++ azure/vnet/vnet.tf
@@ -7,7 +7,7 @@ module "dev_vnet" {
   vnet_name           = "dev-vnet"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3", "subnet4", "subnet5", "subnet6"]
+  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
                           Environment = "dev"
@@ -23,7 +23,7 @@ module "uat_vnet" {
   vnet_name           = "uat-vnet"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3", "subnet4", "subnet5", "subnet6"]
+  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
                           Environment = "uat"
@@ -39,7 +39,7 @@ module "prod_vnet" {
   vnet_name           = "prod-vnet"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3", "subnet4", "subnet5", "subnet6"]
+  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
                           Environment = "prod"
