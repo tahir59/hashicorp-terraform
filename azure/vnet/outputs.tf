85dfeb78312d4ff39a4fe1c28c82239b82028961
tahir59 <52169987+tahir59@users.noreply.github.com>
2019-11-27 20:29:54 +0500

    Update outputs.tf

--- azure/vnet/outputs.tf
+++ azure/vnet/outputs.tf
@@ -1,3 +1,23 @@
+output "prod_vnet_location" {
+  value = module.prod_vnet.vnet_location
+}
+
+output "prod_vnet_name" {
+  value = module.prod_vnet.vnet_name
+}
+
+output "prod_vnet_id" {
+  value = module.prod_vnet.vnet_id
+}
+
+output "prod_vnet_address_space" {
+  value = module.prod_vnet.vnet_address_space
+}
+
+output "prod_vnet_subnets" {
+  value = module.prod_vnet.vnet_subnets
+}
+
 output "dev_vnet_location" {
   value = module.dev_vnet.vnet_location
 }
@@ -18,42 +38,22 @@ output "dev_vnet_subnets" {
   value = module.dev_vnet.vnet_subnets
 }
 
-output "uat_vnet_location" {
-  value = module.uat_vnet.vnet_location
+output "mgmt_vnet_location" {
+  value = module.mgmt_vnet.vnet_location
 }
 
-output "uat_vnet_name" {
-  value = module.uat_vnet.vnet_name
+output "mgmt_vnet_name" {
+  value = module.mgmt_vnet.vnet_name
 }
 
-output "uat_vnet_id" {
-  value = module.uat_vnet.vnet_id
+output "mgmt_vnet_id" {
+  value = module.mgmt_vnet.vnet_id
 }
 
-output "uat_vnet_address_space" {
-  value = module.uat_vnet.vnet_address_space
+output "mgmt_vnet_address_space" {
+  value = module.mgmt_vnet.vnet_address_space
 }
 
-output "uat_vnet_subnets" {
-  value = module.uat_vnet.vnet_subnets
-}
-
-output "prod_vnet_location" {
-  value = module.prod_vnet.vnet_location
-}
-
-output "prod_vnet_name" {
-  value = module.prod_vnet.vnet_name
-}
-
-output "prod_vnet_id" {
-  value = module.prod_vnet.vnet_id
-}
-
-output "prod_vnet_address_space" {
-  value = module.prod_vnet.vnet_address_space
-}
-
-output "prod_vnet_subnets" {
-  value = module.prod_vnet.vnet_subnets
+output "mgmt_vnet_subnets" {
+  value = module.mgmt_vnet.vnet_subnets
 }
