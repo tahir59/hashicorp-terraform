7f8f5a7650e91c153d765c981b8592ae1c199918
tahir59 <52169987+tahir59@users.noreply.github.com>
2019-11-27 20:32:59 +0500

    Update vnet.tf

--- azure/vnet/vnet.tf
+++ azure/vnet/vnet.tf
@@ -1,3 +1,19 @@
+module "prod_vnet" {
+  source              = "Azure/network/azurerm"
+  version             = "2.0.0"
+
+  resource_group_name = "rgroup"
+  location            = "centralus"
+  vnet_name           = "prod-vnet"
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
+  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
+
+  tags                = {
+                          Environment = "prod"
+                        }
+}
+
 module "dev_vnet" {
   source              = "Azure/network/azurerm"
   version             = "2.0.0"
@@ -14,34 +30,18 @@ module "dev_vnet" {
                         }
 }
 
-module "uat_vnet" {
+module "mgmt_vnet" {
   source              = "Azure/network/azurerm"
   version             = "2.0.0"
 
   resource_group_name = "rgroup"
   location            = "centralus"
-  vnet_name           = "uat-vnet"
+  vnet_name           = "mgmt-vnet"
   address_space       = "10.0.0.0/16"
   subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
   subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
-                          Environment = "uat"
-                        }
-}
-
-module "prod_vnet" {
-  source              = "Azure/network/azurerm"
-  version             = "2.0.0"
-
-  resource_group_name = "rgroup"
-  location            = "centralus"
-  vnet_name           = "prod-vnet"
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
-  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
-
-  tags                = {
-                          Environment = "prod"
+                          Environment = "mgmt"
                         }
 }
