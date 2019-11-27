3969b28a72a3b55475ac7fcfb828989e911f16f1
tahir59 <52169987+tahir59@users.noreply.github.com>
2019-11-27 21:13:22 +0500

    Update vnet.tf

--- azure/vnet/vnet.tf
+++ azure/vnet/vnet.tf
@@ -6,7 +6,7 @@ module "prod_vnet" {
   location            = "centralus"
   vnet_name           = "prod-vnet"
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.101.0/24", "10.0.102.0/24", "10.0.201.0/24", "10.0.202.0/24"]
   subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
@@ -22,7 +22,7 @@ module "dev_vnet" {
   location            = "centralus"
   vnet_name           = "dev-vnet"
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
+  subnet_prefixes     = ["20.0.1.0/24", "20.0.2.0/24", "20.0.101.0/24", "20.0.102.0/24", "20.0.201.0/24", "20.0.202.0/24"]
   subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
 
   tags                = {
@@ -38,8 +38,8 @@ module "mgmt_vnet" {
   location            = "centralus"
   vnet_name           = "mgmt-vnet"
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24", "10.0.105.0/24", "10.0.106.0/24"]
-  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04", "subnet-05", "subnet-06"]
+  subnet_prefixes     = ["30.0.1.0/24", "30.0.2.0/24", "30.0.101.0/24", "30.0.102.0/24"]
+  subnet_names        = ["subnet-01", "subnet-02", "subnet-03", "subnet-04"]
 
   tags                = {
                           Environment = "mgmt"
